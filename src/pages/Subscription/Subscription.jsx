9defd08b0054aaa07e85481d1ec30820bfabc168
Ben Bishop <ben.bishop@purplebricks.com>
2019-08-30 11:06:45 +1000

    Added data to json
    Without an API to pull the data this felt the cleanest

--- src/pages/Subscription/Subscription.jsx
+++ src/pages/Subscription/Subscription.jsx
@@ -3,6 +3,10 @@ import { css, jsx } from "@emotion/core";
 import { Fragment, useState } from "react";
 
 import SubscriptionContext from "../../context/SubscriptionContext";
+import {
+  plans as dataPlans,
+  addOns as dataAddOns
+} from "../../data/plansAndAddOns.json";
 
 import BackButton from "../../components/BackButton/BackButton";
 import Plans from "../../components/Plans/Plans";
@@ -10,43 +14,8 @@ import AddOns from "../../components/AddOns/AddOns";
 import Summary from "../../components/Summary/Summary";
 
 const Subscription = () => {
-  const [plans] = useState([
-    {
-      name: "Silver",
-      description: "2,000 contacts monthly"
-    },
-    {
-      name: "Gold",
-      description: "5,000 contacts monthly"
-    },
-    {
-      name: "Platinum",
-      description: "10,000 contacts monthly"
-    },
-    {
-      name: "Platinum+",
-      description: "40,000 contacts monthly"
-    }
-  ]);
-
-  const [addOns] = useState([
-    {
-      name: "Priority support",
-      price: "$199/month"
-    },
-    {
-      name: "Salesforce CRM integration",
-      price: "$199/month"
-    },
-    {
-      name: "Custom IP",
-      price: "$149/month"
-    },
-    {
-      name: "Activity streams",
-      price: "$149/month"
-    }
-  ]);
+  const [plans] = useState(dataPlans);
+  const [addOns] = useState(dataAddOns);
 
   const [activePlan, setActivePlan] = useState("Platinum+");
   const [activeAddOns, setActiveAddOns] = useState(["Priority support"]);
